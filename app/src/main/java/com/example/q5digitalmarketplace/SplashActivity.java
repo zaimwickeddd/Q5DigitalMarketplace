7fcade436da2b98b5542633d305e48534d4065cb
Nadzim <abgmnadzim07@gmail.com>
2026-06-23 14:41:22 +0800

    commit splash, welcome, login, signup

---
+++ app/src/main/java/com/example/q5digitalmarketplace/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.q5digitalmarketplace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
